0f27da49e97ddd4b9ef368db62330e47045bf53f
matejmecka <matej.plavevski+github@gmail.com>
2021-01-30 13:47:33 +0100

    idk what was i doing

---
+++ Assets/Creator Kit - RPG/Scripts/Gameplay/Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+	public float speed;
+    private Transform target;
+    public GameObject player;
+    public float randomNumber;
+
+    PlayerHealth playerHealth;
+
+    // Sound
+	AudioSource enemyAudioSource;
+    public AudioClip attackClip;
+
+
+    public float health = 100;
+    public float timer;
+
+    // Attack stuff
+
+    public int attackDamage = 20;
+    public float betweenAttacks = 3f;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        target = player.GetComponent<Transform>();
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyAudioSource = GetComponent<AudioSource>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, target.position) > 0 && Vector2.Distance(transform.position, target.position) < 30){
+            transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        }	
+
+        if (Vector2.Distance(transform.position, target.position) < 5){
+            if (timer >= betweenAttacks && health > 0){
+                attack();
+            }
+        }
+
+
+    }
+
+    void attack(){
+
+        if(playerHealth.currentHealth > 0){
+            randomNumber = Random.Range(0, 25);
+            enemyAudioSource.clip = attackClip;
+            enemyAudioSource.Play();
+            if (randomNumber % 2 == 0){
+                playerHealth.TakeDamage(attackDamage);      
+            }
+            timer = 0;
+        }
+
+    }
+
+
+}
